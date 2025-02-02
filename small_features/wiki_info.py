5a815a0dfb05db245ef6a655479aaf73a64f469e
dannyone <danilbadarev@gmail.com>
2025-02-02 14:01:00 +0100

    wiki: wiki function created

---
+++ small_features/wiki_info.py
@@ -0,0 +1,21 @@
+import wikipedia as wiki
+
+def wiki_search(query, lang='en'):
+
+    try:
+        wiki.set_lang(lang)
+        page = wiki.page(query)
+    except wiki.PageError:
+        return f"Sorry, I couldn't find anything about <{query}>\n" \
+               f"Please try it yourself: https://en.wikipedia.org/wiki/{query}"
+    except wiki.DisambiguationError:
+        return f"Sorry, your query is too vague, make it more clear and ask again!\n" \
+               f"Or you can just try to find it yourself: https://en.wikipedia.org/wiki/{query}"
+    short_sum = page.summary.split('\n')[0]
+    title = f"< {page.title.upper()} >"
+    link = f"For more info: {page.url}"
+    return "\n\n".join((title, short_sum, link))
+
+
+if __name__ == '__main__':
+    print(wiki_search('gun'))

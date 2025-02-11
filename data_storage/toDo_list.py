136f9c1af719cc94d462228a4a26ca712a348fcf
dannyone <danilbadarev@gmail.com>
2025-02-11 22:10:46 +0100

    daily_planner: functions get important task and upgrade progress added

--- data_storage/toDo_list.py
+++ data_storage/toDo_list.py
@@ -1,8 +1,11 @@
 import json
+from datetime import datetime, timedelta
+
 from config import PLANNER_PATH
 
+
 VK_TASK = "{num}. {text}\n" \
-          "{sign} needs to be finished before {date} (progress: {prog})\n"
+          "{sign} Deadline: {date} (progress: {prog})\n"
 
 def get_to_do(person_id):
     with open(f"{PLANNER_PATH}/to_do.json", "r") as file:
@@ -31,17 +34,37 @@ def get_to_do(person_id):
 def get_to_do_important(person_id):
     with open(f"{PLANNER_PATH}/to_do.json", "r") as file:
         all_planners = json.load(file)
-    planner = all_planners.get(person_id)
+    planner = all_planners.get(str(person_id))
     if not planner:
         return False
 
     important = []
     close_date = []
-    # for task in planner:
-    #     sign = '*'
-    #     if task['important']:
-    #         important.append(VK_TASK.format(num=1, text=task['text'], sign=sign, date=task['date'], prog=task['progress']))
+    for i in range(len(planner)):
+        task = planner[i]
+        time_left = None
+        if task['date'] != "<no_deadline>":
+            time_left = datetime.strptime(task['date']+f".{datetime.now().year}", '%d.%m.%Y') - datetime.now()
+            if time_left < timedelta(days=0):
+                remove_task(person_id, i+1)         # if the deadline for task has passed, delete task
+                continue
+        # print(time_left)
 
+        # the representation differs if the task is close to deadline
+        sign, date = ('🔥', f"🚨 {task['date']} 🚨") if time_left and time_left < timedelta(days=2) else ('*', task['date'])
+
+        if task['important']:
+            important.append(VK_TASK.format(num=i+1, text=task['text'], sign=sign, date=date, prog=task['progress']))
+            continue
+        if sign != '*':             # it means the deadline is closed
+            close_date.append(VK_TASK.format(num=i+1, text=task['text'], sign='🔥', date=date, prog=task['progress'] ))
+
+    return f"""
+=====================
+⚠️ IMPORTANT TASK ⚠️
+{''.join(important)}=====================
+    
+{''.join(close_date)}"""
 
 def add_task(person_id, text, topic=None, date=None, progress=None, imp=False):
     with open(f"{PLANNER_PATH}/to_do.json", "r", encoding='utf-8') as file:
@@ -55,7 +78,7 @@ def add_task(person_id, text, topic=None, date=None, progress=None, imp=False):
         'text': text,
         'topic': topic.upper() if topic else 'GENERAL',
         'date': date if date else '<no_deadline>',
-        'progress': progress if progress else '🟥',
+        'progress': progress if progress else '&#128997;',
         'important': imp
     }
     planner.append(task)
@@ -81,10 +104,39 @@ def remove_task(person_id, task_id):
     return True
 
 
+def upgrade_task_progress(person_id, task_id):
 
+    progress_converter = {
+        '&#128997;': '&#128999;',
+        '&#128999;': '&#129000;',
+        '&#129000;': '&#129001;',
+        '&#129001;': '&#128997;'
+    }
+    with open(f"{PLANNER_PATH}/to_do.json", "r", encoding='utf-8') as file:
+        all_planners = json.load(file)
+
+    planner = all_planners.get(str(person_id))
+    # print(planner)
+    if not planner or len(planner) < task_id or task_id < 0:
+        return False
+
+    planner[task_id-1]['progress'] = progress_converter[planner[task_id-1]['progress']]     # proceed to the next stage
+
+    with open(f"{PLANNER_PATH}/to_do.json", "w", encoding='utf-8') as file:
+        json.dump(all_planners, file, indent=4, ensure_ascii=False)
+
+    return True
 
 
 if __name__ == '__main__':
-    # add_task(587938956, 'do another math', topic='math', imp=True)
+    # add_task(587938956, 'do another math', topic='math')
+    # add_task(587938956, 'kill myself', date='15.02', imp=True)
+    # add_task(587938956, 'buy phone', date='12.02')
     # print(remove_task(587938956, 3))
+    # print(get_to_do_important(587938956))
+    # print(upgrade_task_progress(587938956, 10))
+    # print(upgrade_task_progress(587938956, 4))
+    # print(get_to_do_important(587938956))
+
+
     print("DONE")
